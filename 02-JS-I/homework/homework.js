8ee00065378989ceba4f19be104710116eb19f66
JuanmaZalazar <Juanmanuelzalazar@outlook.com>
2022-01-10 22:58:27 -0300

    Finish homework 02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -238,8 +238,8 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-else if (letra =='a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
-  return “Es vocal”
+if ( letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ) {
+  return 'Es vocal'
 }
 else{
   return "Dato incorrecto"
